8b2b8106bc9dcc92f5c5bcf2ee32a0b9e2fb0659
Fred Hickernell <hickernell@iit.edu>
2020-05-25 08:41:42 -0500

    adding more

--- ProgramsImages/EllipsesPlot.m
+++ ProgramsImages/EllipsesPlot.m
@@ -16,30 +16,46 @@ spherevec = [cos(phiplot) sin(phiplot)];
 xdata = [0.3; 0.6];
 ydata = f(xdata);
 
-figure
+efigh = figure;
+hold on
+kfigh = figure;
 hold on
 colorScheme = [MATLABBlue; MATLABOrange; MATLABGreen; MATLABPurple];
-thetavec = [0.5 10 2]';
+cOrder = [1 3 2 4];
+thetavec = [0.5 8 2]';
 thOrder = [1 3 2];
 ntheta = size(thetavec,1);
 h=zeros(ntheta,1);
+hK=zeros(ntheta,1);
 legendLabel = cell(length(thetavec));
 for i = 1:ntheta
    theta = thetavec(i);
    Kmat = MaternKernel(xdata,xdata,theta);
    L = chol(Kmat,'lower');
    ellipsevec = sqrt(ydata'*(Kmat\ydata))*spherevec*L';
-   h(i)= fill(ellipsevec(:,1), ellipsevec(:,2), colorScheme(i,:), ...
-      'EdgeColor', colorScheme(i,:));
+   figure(efigh);
+   h(i)= fill(ellipsevec(:,1), ellipsevec(:,2), colorScheme(cOrder(i),:), ...
+      'EdgeColor', colorScheme(cOrder(i),:));
    legendLabel{i} = ['\(\theta = ' num2str(theta) '\)'];
+   Kplot = MaternKernel(xplot,0.5,theta);
+   figure(kfigh);
+   hK(i)=plot(xplot,Kplot,'-','color',colorScheme(cOrder(i),:));
 end
+figure(efigh);
 plot(ydata(1),ydata(2),'k.');
 xlabel('\(z_1\)')
 ylabel('\(z_2\)')
 legend(h(thOrder),legendLabel{thOrder},  ...
    'location','northwest','box','off');
 print -depsc ellipsesPlot.eps
- 
+
+figure(kfigh);
+xlabel('\(x\)')
+ylabel('\(K_{\theta}(x,0.5)\)')
+legend(hK(thOrder),legendLabel{thOrder},  ...
+   'location','south','box','off');
+print -depsc KthetaPlot.eps
+
 
 
 
