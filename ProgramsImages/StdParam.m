3de0655e2cbf9cca005f3186aa3e61f0a77f2577
Fred Hickernell <hickernell@iit.edu>
2020-06-13 09:24:20 -0500

    forgot one program

---
+++ ProgramsImages/StdParam.m
@@ -0,0 +1,11 @@
+function [thetavec,nth,xplot,nxplot,Ainf,B0] = ...
+   StdParam
+%STDPARAM sets the standard parameters for our examples
+thetavec = [1 4 16]';
+nth = size(thetavec,1);
+xplot = (0:0.001:1)';
+nxplot = size(xplot,1);
+Ainf = 0.2;
+B0 = 0.5;
+end
+
