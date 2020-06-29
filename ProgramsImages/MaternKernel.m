b7754d0e579157c886dd41d82a72d8910408057d
Fred J Hickernell <hickernell@iit.edu>
2020-06-29 17:53:19 -0500

    Fix Matern

--- ProgramsImages/MaternKernel.m
+++ ProgramsImages/MaternKernel.m
@@ -10,20 +10,20 @@ function kerval = MaternKernel(t,x,theta)
     end
     
     if (dth == d)
-    tmx = (reshape(t,[nt,1,d]) - reshape(x,[1,nx,d])) ...
-        .* reshape(theta,[1 1 dth]);
-    normtmx = reshape(sqrt(sum(tmx.^2,3)),[nt,nx]);
-    kerval = (1 + normtmx) .*  exp(-normtmx);
+       tmx = (reshape(t,[nt,1,d]) - reshape(x,[1,nx,d])) ...
+           .* reshape(theta,[1 1 dth]);
+       normtmx = reshape(sqrt(sum(tmx.^2,3)),[nt,nx]);
+       kerval = (1 + normtmx) .*  exp(-normtmx);
     else
-    dd = dth/2;
-    thetaa = theta(1:dd);
-    thetab = theta(dd+1);
-    tmx = (reshape(t,[nt,1,d]) - reshape(x,[1,nx,d])) ...
-        .* reshape(thetaa,[1 1 dd]);
-    normtmx = reshape(sqrt(sum(tmx.^2,3)),[nt,nx]);
-    tmpl = reshape(sum((reshape(t,[nt,1,d]) + reshape(x,[1,nx,d]))...
-        .* reshape(thetab,[1 1 dd]),3),[nt,nx]);
-    kerval = exp(tmpl).*(1 + normtmx) .*  exp(-normtmx); 
+       dd = dth/2;
+       thetaa = theta(1:dd);
+       thetab = theta(dd+1:dth);
+       tmx = (reshape(t,[nt,1,d]) - reshape(x,[1,nx,d])) ...
+           .* reshape(thetaa,[1 1 dd]);
+       normtmx = reshape(sqrt(sum(tmx.^2,3)),[nt,nx]);
+       tmpl = reshape(sum((reshape(t,[nt,1,d]) + reshape(x,[1,nx,d]))...
+           .* reshape(thetab,[1 1 dd]),3),[nt,nx]);
+       kerval = exp(tmpl).*(1 + normtmx) .*  exp(-normtmx); 
     end
 end
 
