ef637fbc681ca8a40e500976d96193307cf6a5b3
yding <jinliang98@gmail.com>
2020-09-17 14:49:14 -0500

    Update LimFunExample

--- ProgramsImages/LimFunExample.m
+++ ProgramsImages/LimFunExample.m
@@ -3,9 +3,9 @@ S = struct('type','{}','subs',{{':'}}); prm=[];
 [prm.kername] = subsref({'SpatialMatern'},S);
 [prm,kernelth] = parseFunAppxParam(prm);
 nAlg = size(prm,2);
-%f = @(x) 1/6*((30+5*x(:,1).*sin(5*x(:,1))).*...
-%    (4+exp(-5*x(:,2)))-100);
-f = @(x) cos(sum(x,2)).*exp(prod(x,2));
+f = @(x) 1/6*((30+5*x(:,1).*sin(5*x(:,1))).*...
+    (4+exp(-5*x(:,2)))-100);
+%f = @(x) cos(sum(x,2)).*exp(prod(x,2));
 [prm.fname] = subsref(repmat({'Lim2DFun'},1,nAlg),S);
 %[prm.kername] = subsref({'SpatialMatern'},S);
 [prm.n0] = subsref({10},S);
@@ -17,8 +17,8 @@ xRange = (-5:0.5:5)';
 [prm.yLim] = subsref(repmat({[-0.2;0.5]},1,nAlg),S);
 [prm.legendPos] = subsref(repmat({'northeast'},1,nAlg),S);
 [prm.plotSites] = subsref({false},S);
-[prm.abstolVec] = subsref({0.1},S);
-[prm.nmax] = subsref({500},S);
+[prm.abstolVec] = subsref({[0.05 0.02 0.01]},S);
+[prm.nmax] = subsref({2000},S);
 [prm.canvasTheta]= subsref({false},S);
 [prm.currentTheta]= subsref({[0 0 0 0]},S);
 [prm.whDes] = subsref({'adapt_th'},S);
