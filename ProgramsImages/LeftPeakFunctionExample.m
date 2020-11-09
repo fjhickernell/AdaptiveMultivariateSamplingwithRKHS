c812a91a52c6a97f2fcce8dd415e88cd73ffe503
Fred J Hickernell <hickernell@iit.edu>
2020-11-09 10:23:10 -0600

    Office Computer update

--- ProgramsImages/LeftPeakFunctionExample.m
+++ ProgramsImages/LeftPeakFunctionExample.m
@@ -1,8 +1,12 @@
 %% Left Peak Function Example
 leftPeakEx = FunctionApproxProblem(repmat({@LeftPeakFun},1,2));
 leftPeakEx = set_prop(leftPeakEx,'Algo',{@AdaptAlgo2,@AdaptAlgo3});
-leftPeakEx = set_prop(leftPeakEx,'n0',{1,10});
-leftPeakEx = set_prop(leftPeakEx,'yLim',{[-0.5;0.5]});
+leftPeakEx = set_prop(leftPeakEx,'n0',{10});
+leftPeakEx = set_prop(leftPeakEx,'yLim',{[-0.4;0.5]});
 leftPeakEx = set_prop(leftPeakEx,'legendPos',{'northeast'});
 %%
-OutLeftPeak = RunFunAppxExample(leftPeakEx);
+OutLeftPeakGauss = RunFunAppxExample(leftPeakEx);
+
+%%
+leftPeakEx = set_prop(leftPeakEx,'kernelOrig',{@MaternKernel});
+OutLeftPeakMatern = RunFunAppxExample(leftPeakEx);
