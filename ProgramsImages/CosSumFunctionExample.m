97d7058d6e6699c99fe2794212828a967ad45276
Fred J Hickernell <hickernell@iit.edu>
2020-11-27 14:36:54 -0600

    Office Computer update

--- ProgramsImages/CosSumFunctionExample.m
+++ ProgramsImages/CosSumFunctionExample.m
@@ -2,6 +2,7 @@
 tic
 clearvars
 cosSumFunEx = FunctionApproxProblem(repmat({@CosSumFun},1,1));
+rng(47)
 d = 12;
 fpar = 2.^(-1:-1:-d);
 fpar = fpar(1,randperm(d));
@@ -9,22 +10,22 @@ cosSumFunEx = set_prop(cosSumFunEx,'fparam',{fpar});
 cosSumFunEx = set_prop(cosSumFunEx,'whDes',{'lattice'});
 cosSumFunEx = set_prop(cosSumFunEx,'whObj',{'EmpBayes'});
 %cosSumFunEx = set_prop(cosSumFunEx,'whDes',{'apdapt_th'});
-cosSumFunEx = set_prop(cosSumFunEx,'nmax',{200});
+cosSumFunEx = set_prop(cosSumFunEx,'nmax',{50});
 cosSumFunEx = set_prop(cosSumFunEx,'theta',{-3*ones(1,d)});
 cosSumFunEx = set_prop(cosSumFunEx,'xLim',{[zeros(1,d); ones(1,d)]});
 cosSumFunEx = set_prop(cosSumFunEx,'Algo',{@AdaptAlgo3});
-cosSumFunEx = set_prop(cosSumFunEx,'n0',{2^5});
+cosSumFunEx = set_prop(cosSumFunEx,'n0',{2^4});
 cosSumFunEx = set_prop(cosSumFunEx,'yLim',{[-1;1]});
 cosSumFunEx = set_prop(cosSumFunEx,'legendPos',{'eastoutside'});
 cosSumFunEx = set_prop(cosSumFunEx,'abstolVec',{[0.1 0.05 0.02 0.01 0.005 0.002 0.001]'});
 %%
-cosSumFunEx = set_prop(cosSumFunEx,'neval',{2^16});
+cosSumFunEx = set_prop(cosSumFunEx,'neval',{2^14});
 [OutCosSumGauss,cosSumFunEx] = RunFunAppxExample(cosSumFunEx);
 
 cosSumFunEx = set_prop(cosSumFunEx,'Algo',{@AdaptAlgo2});
 cosSumFunEx = set_prop(cosSumFunEx,'theta',{-3*ones(1,d)});
 cosSumFunEx = set_prop(cosSumFunEx,'abstolVec',{[0.1 0.05 0.02 0.01]'});
-cosSumFunEx = set_prop(cosSumFunEx,'nmax',{500});
+cosSumFunEx = set_prop(cosSumFunEx,'nmax',{50});
 [OutCosSumGaussMatern,cosSumFunEx] = RunFunAppxExample(cosSumFunEx);
 %  cosSumFunEx = set_prop(cosSumFunEx,'kernelOrig',{@MaternKernelGeneral});
 %  cosSumFunEx = set_prop(cosSumFunEx,'theta',{[0.4 -ones(1,d)]});
