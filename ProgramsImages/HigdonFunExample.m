a244f5f82018b211d6be44312b8e03f15fbf2193
yding <jinliang98@gmail.com>
2021-03-09 16:12:38 -0600

    Updates

--- ProgramsImages/HigdonFunExample.m
+++ ProgramsImages/HigdonFunExample.m
@@ -4,8 +4,8 @@ clearvars
 HigdonFunEx = FunctionApproxProblem(repmat({@HigdonFun},1,3));
 %simpleFunEx = set_prop(simpleFunEx,'kernelOrig',{@MaternKernel});
 HigdonFunEx = set_prop(HigdonFunEx,'Algo',{@AdaptAlgo1,@AdaptAlgo2,@AdaptAlgo3});
-HigdonFunEx = set_prop(HigdonFunEx,'theta',{1 , 1 , 1});
-HigdonFunEx = set_prop(HigdonFunEx,'n0',repmat({1},1,3));
+HigdonFunEx = set_prop(HigdonFunEx,'theta',{0, 0, 1});
+HigdonFunEx = set_prop(HigdonFunEx,'n0',repmat({5},1,3));
 HigdonFunEx = set_prop(HigdonFunEx,'nmax',repmat({100},1,3));
 HigdonFunEx = set_prop(HigdonFunEx,'yLim',repmat({[-1.5;1.5]},1,3));
 HigdonFunEx = set_prop(HigdonFunEx,'abstolVec',repmat({[0.01 0.001 0.0001 0.00001]'},1,3));
@@ -13,11 +13,20 @@ HigdonFunEx = set_prop(HigdonFunEx,'abstolVec',repmat({[0.01 0.001 0.0001 0.0000
 %%
 [OutHigdonFunGaussOptimTheta,HigdonFunEx] = RunFunAppxExample(HigdonFunEx);
 
-%% With uniform grid 
-%HigdonFunEx = set_prop(HigdonFunEx,'whDes',{'unif_grid'});
-%simpleFunOutUnifGrid = RunFunAppxExample(HigdonFunEx);
-
 toc
 
+%%
+clearvars
+HigdonFunEx = FunctionApproxProblem({@HigdonFun});
+%simpleFunEx = set_prop(simpleFunEx,'kernelOrig',{@MaternKernel});
+HigdonFunEx = set_prop(HigdonFunEx,'Algo',{@AdaptAlgo1});
+HigdonFunEx = set_prop(HigdonFunEx,'theta',{0});
+HigdonFunEx = set_prop(HigdonFunEx,'n0',{1});
+HigdonFunEx = set_prop(HigdonFunEx,'nmax',{100});
+HigdonFunEx = set_prop(HigdonFunEx,'yLim',{[-1.5;1.5]});
+HigdonFunEx = set_prop(HigdonFunEx,'abstolVec',{[0.01 0.001 0.0001 0.00001]'});
+
+%%
+[OutHigdonFunGaussOptimTheta,HigdonFunEx] = RunFunAppxExample(HigdonFunEx);
 
    
